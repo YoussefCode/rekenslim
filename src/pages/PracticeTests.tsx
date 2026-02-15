f998d529c07b66d9b2ff6f9360c4932cdac8caf0
rekenslim <webmaster@rekenslim.nl>
2026-02-15 23:20:24 +0100

    Add PracticeTests component for oefentoets

---
+++ src/pages/PracticeTests.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PracticeTests = () => {
+    return (
+        <div>
+            <h1>Oefentoets</h1>
+            <p>This is a practice test component.</p>
+        </div>
+    );
+};
+
+export default PracticeTests;
